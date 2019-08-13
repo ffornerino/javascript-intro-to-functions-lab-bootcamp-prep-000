047a18cb9338d8df10114033753138921cb5c877
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-13 18:40:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function whisper(string){
 
 function logShout(string){
    var uppercase = string.toUpperCase()
-   return console.log(string)
+   return console.log(uppercase)
 }
  
 
 function logWhisper(string){
   var lowercase = string.toLowerCase()
-  return console.log(string)
+  return console.log(lowercase)
 }
 
 /*function sayHiToGrandma(string){
