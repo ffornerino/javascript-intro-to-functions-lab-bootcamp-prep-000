f92a19cd769d03af6f17940504f75ca4904fa051
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-08-13 19:01:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,11 +22,19 @@ function sayHiToGrandma(string){
   var lowercase = string.toLowerCase()
   var iLoveYou  = "I love you, Grandma."
   
-  if (lowercase === string){
+ if (lowercase === string){
+   return `I can\'t hear you!`
+  } else if (uppercase === string){
+    return `YES INDEED!`
+  } else if (iLoveYou === string) {
+    return `I love you, too.`
+  } 
+  
+/*  if (lowercase === string){
    return console.log("I can't hear you!")
   } else if (uppercase === string){
     return console.log("YES INDEED!")
   } else if (iLoveYou === string) {
     return console.log("I love you, too.")
-  } 
+  } */
 }
